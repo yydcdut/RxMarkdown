77e4d247eff4882f28160fc49f8fe0fa234c0d03
yuyidong <378040621@qq.com>
2016-07-05 14:20:57 +0800

    fix bug

--- rxmarkdown/src/main/java/com/yydcdut/rxmarkdown/grammar/edit/InlineCodeGrammar.java
+++ rxmarkdown/src/main/java/com/yydcdut/rxmarkdown/grammar/edit/InlineCodeGrammar.java
@@ -27,7 +27,7 @@ class InlineCodeGrammar extends EditGrammarAdapter {
     @Override
     public List<EditToken> format(@NonNull Editable editable) {
         List<EditToken> editTokenList = new ArrayList<>();
-        StringBuilder content = new StringBuilder(editable.toString().replace("```", "  "));
+        StringBuilder content = new StringBuilder(editable.toString().replace("```", "   "));
         Pattern p = Pattern.compile("(`)(.+?)(`)");
         Matcher m = p.matcher(content);
         List<String> matchList = new ArrayList<>();//找到的
