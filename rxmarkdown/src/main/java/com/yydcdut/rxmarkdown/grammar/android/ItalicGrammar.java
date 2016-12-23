9cbeeafead4bab1bd626568ef33b3eceaab9de01
yuyidong <378040621@qq.com>
2016-12-23 15:50:31 +0800

    It still has problems,so revert

--- rxmarkdown/src/main/java/com/yydcdut/rxmarkdown/grammar/android/ItalicGrammar.java
+++ rxmarkdown/src/main/java/com/yydcdut/rxmarkdown/grammar/android/ItalicGrammar.java
@@ -15,15 +15,14 @@
  */
 package com.yydcdut.rxmarkdown.grammar.android;
 
-import com.yydcdut.rxmarkdown.RxMDConfiguration;
-
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.text.Spannable;
 import android.text.SpannableStringBuilder;
-import android.text.Spanned;
 import android.text.style.StyleSpan;
 
-import java.util.regex.Matcher;
+import com.yydcdut.rxmarkdown.RxMDConfiguration;
+
 import java.util.regex.Pattern;
 
 import static com.yydcdut.rxmarkdown.grammar.android.BackslashGrammar.KEY_BACKSLASH;
@@ -40,10 +39,9 @@ class ItalicGrammar extends AbsAndroidGrammar {
 
     protected static final String KEY_ITALIC = "*";
     protected static final String KEY_ITALIC_1 = "_";
+
     protected static final String KEY_BACKSLASH_VALUE = KEY_BACKSLASH + KEY_ITALIC;
     protected static final String KEY_BACKSLASH_VALUE_1 = KEY_BACKSLASH + KEY_ITALIC_1;
-    private static final Pattern italicsMatchesPattern = Pattern.compile(".*([\\W&&[^\\\\]]|^)(\\*|_)(\\S.*?\\S)(\\2)(\\s|$|[.,!?:\\(\\)]).*");
-    private static final Pattern italicsPattern = Pattern.compile("([\\W&&[^\\\\]]|^)(\\*|_)(\\S.*?\\S)(\\2)(\\s|$|[.,!?:\\(\\)])");
 
     ItalicGrammar(@NonNull RxMDConfiguration rxMDConfiguration) {
         super(rxMDConfiguration);
@@ -51,36 +49,132 @@ class ItalicGrammar extends AbsAndroidGrammar {
 
     @Override
     boolean isMatch(@NonNull String text) {
-        return italicsMatchesPattern.matcher(text).matches();
+        if (!text.contains(KEY_ITALIC) && !text.contains(KEY_ITALIC_1)) {
+            return false;
+        }
+        boolean match = false;
+        Pattern pattern = Pattern.compile(".*[\\*]{1}.*[\\*]{1}.*");
+        Pattern pattern1 = Pattern.compile(".*[_]{1}.*[_]{1}.*");
+        match |= pattern.matcher(text).matches();
+        if (match) {
+            return true;
+        }
+        match |= pattern1.matcher(text).matches();
+        return match;
     }
 
     @NonNull
     @Override
     SpannableStringBuilder encode(@NonNull SpannableStringBuilder ssb) {
+        int index;
+        while (true) {
+            String text = ssb.toString();
+            index = text.indexOf(KEY_BACKSLASH_VALUE);
+            if (index == -1) {
+                break;
+            }
+            ssb.replace(index, index + KEY_BACKSLASH_VALUE.length(), BackslashGrammar.KEY_ENCODE);
+        }
+        while (true) {
+            String text = ssb.toString();
+            index = text.indexOf(KEY_BACKSLASH_VALUE_1);
+            if (index == -1) {
+                break;
+            }
+            ssb.replace(index, index + KEY_BACKSLASH_VALUE_1.length(), BackslashGrammar.KEY_ENCODE_1);
+        }
         return ssb;
     }
 
     @NonNull
     @Override
     SpannableStringBuilder format(@NonNull SpannableStringBuilder ssb) {
-        Matcher matcher = italicsPattern.matcher(ssb);
-
-        while (matcher.find()) {
-            if (!checkInInlineCode(ssb, matcher.start(2), matcher.start(4))) {
-                ssb.setSpan(new StyleSpan(Typeface.ITALIC), matcher.start(2), matcher.start(4), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                ssb.delete(matcher.start(4), matcher.end(4));
-                ssb.delete(matcher.start(2), matcher.end(2));
-                matcher.reset(ssb);
-            }
-        }
-
-        return ssb;
+        ssb = parse(KEY_ITALIC, ssb.toString(), ssb);
+        return parse(KEY_ITALIC_1, ssb.toString(), ssb);
     }
 
     @NonNull
     @Override
     SpannableStringBuilder decode(@NonNull SpannableStringBuilder ssb) {
+        int index;
+        while (true) {
+            String text = ssb.toString();
+            index = text.indexOf(BackslashGrammar.KEY_ENCODE);
+            if (index == -1) {
+                break;
+            }
+            ssb.replace(index, index + BackslashGrammar.KEY_ENCODE.length(), KEY_BACKSLASH_VALUE);
+        }
+        while (true) {
+            String text = ssb.toString();
+            index = text.indexOf(BackslashGrammar.KEY_ENCODE_1);
+            if (index == -1) {
+                break;
+            }
+            ssb.replace(index, index + BackslashGrammar.KEY_ENCODE_1.length(), KEY_BACKSLASH_VALUE_1);
+        }
         return ssb;
     }
 
+    /**
+     * parse
+     *
+     * @param key  {@link ItalicGrammar#KEY_ITALIC} or {@link ItalicGrammar#KEY_ITALIC_1}
+     * @param text the original content,the class type is {@link String}
+     * @param ssb  the original content,the class type is {@link SpannableStringBuilder}
+     * @return the content after parsing
+     */
+    @NonNull
+    private SpannableStringBuilder parse(@NonNull String key, @NonNull String text, @NonNull SpannableStringBuilder ssb) {
+        SpannableStringBuilder tmp = new SpannableStringBuilder();
+        String tmpTotal = text;
+        while (true) {
+            int positionHeader = findPosition(key, tmpTotal, ssb, tmp);
+            if (positionHeader == -1) {
+                tmp.append(tmpTotal.substring(0, tmpTotal.length()));
+                break;
+            }
+            tmp.append(tmpTotal.substring(0, positionHeader));
+            int index = tmp.length();
+            tmpTotal = tmpTotal.substring(positionHeader + key.length(), tmpTotal.length());
+            int positionFooter = findPosition(key, tmpTotal, ssb, tmp);
+            if (positionFooter != -1) {
+                ssb.delete(tmp.length(), tmp.length() + key.length());
+                tmp.append(tmpTotal.substring(0, positionFooter));
+                ssb.setSpan(new StyleSpan(Typeface.ITALIC), index, tmp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                ssb.delete(tmp.length(), tmp.length() + key.length());
+            } else {
+                tmp.append(key);
+                tmp.append(tmpTotal.substring(0, tmpTotal.length()));
+                break;
+            }
+            tmpTotal = tmpTotal.substring(positionFooter + key.length(), tmpTotal.length());
+        }
+        return ssb;
+    }
+
+    /**
+     * find the position of next "*"
+     * ignore the "*" in inline code grammar
+     *
+     * @param tmpTotal the original content, the class type is {@link String}
+     * @param ssb      the original content, the class type is {@link SpannableStringBuilder}
+     * @param tmp      the content that has parsed
+     * @return the next position of "*"
+     */
+    private int findPosition(@NonNull String key, @NonNull String tmpTotal, @NonNull SpannableStringBuilder ssb, @NonNull SpannableStringBuilder tmp) {
+        String tmpTmpTotal = tmpTotal;
+        int position = tmpTmpTotal.indexOf(key);
+        if (position == -1) {
+            return -1;
+        } else {
+            if (checkInInlineCode(ssb, tmp.length() + position, key.length())) {//key是否在inlineCode中
+                StringBuilder sb = new StringBuilder(tmpTmpTotal.substring(0, position))
+                        .append("$").append(tmpTmpTotal.substring(position + key.length(), tmpTmpTotal.length()));
+                return findPosition(key, sb.toString(), ssb, tmp);
+            } else {
+                return position;
+            }
+        }
+    }
 }
