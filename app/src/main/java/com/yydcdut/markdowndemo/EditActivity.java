3e3b04bf11426f609972c1c5f8802e27afeb86da
yuyidong <378040621@qq.com>
2017-03-15 23:47:59 +0800

    delete log

--- app/src/main/java/com/yydcdut/markdowndemo/EditActivity.java
+++ app/src/main/java/com/yydcdut/markdowndemo/EditActivity.java
@@ -9,9 +9,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -106,24 +103,6 @@ public class EditActivity extends AppCompatActivity implements View.OnClickListe
                 });
 
         mAsyncTask = new EditActivity.DemoPictureAsyncTask().execute();
-
-
-        mEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int before, int after) {
-                Log.i("yuyidong", "beforeTextChanged  start-->" + start + "  before-->" + before + "  after-->" + after);
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int after) {
-                Log.i("yuyidong", "onTextChanged  start-->" + start + "  before-->" + before + "  after-->" + after);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
     }
 
 
